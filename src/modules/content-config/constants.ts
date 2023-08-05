921a9ae31bb63f9a6e2ed6255ab60596f1690f1f
Yair Cohen <yair.cohen@coralogix.com>
2023-08-05 09:40:27 +0300

    fix default config file names

--- src/modules/content-config/constants.ts
+++ src/modules/content-config/constants.ts
@@ -3,5 +3,5 @@ export const DEFAULT_CONFIG_FILE_NAME = "contentbuddy.config.json";
 export const CONFIG_FILE_NAMES = [
   DEFAULT_CONFIG_FILE_NAME,
   "contentbuddy.config.js",
-  "contentbuddy.config.js",
+  "contentbuddy.config.ts",
 ];
